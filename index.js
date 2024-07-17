93e01fb5c0c19f77e2bcc8b9ee67262582295205
antonia trombetta <antonia.dorazio@gmail.com>
2024-07-17 16:19:55 +0200

    lab done

--- index.js
+++ index.js
@@ -9,24 +9,29 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+let concatenateString = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5;
 
 // Print out the concatenated string
 
-
+console.log(concatenateString);
 
 
 /*******************************************
     Iteration 1.2 | Camel Tail
 *******************************************/
+
 const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+const uppercase1 = part1.slice(0, -1) + part1.slice( -1).toUpperCase();
+const uppercase2 = part2.slice(0, -1) + part2.slice( -1).toUpperCase();
+const concatenateString = uppercase1 + " " + uppercase2;
 
 // Print the cameLtaiL-formatted string
 
-
+console.log(uppercase1, uppercase2);
 
 
 /*******************************************
@@ -36,10 +41,11 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+const tip = billTotal * 0.15;
 
 // Print out the tipAmount
 
-
+console.log(tip);
 
 
 /*******************************************
@@ -48,9 +54,12 @@ const billTotal = 84;
 
 // Generate a random integer between 1 and 10 (inclusive)
 
+const randomNummer = Math.floor(Math.random() * 10) + 1;
+
 
 // Print the generated random number
 
+console.log(randomNummer);
 
 
 /*******************************************
@@ -61,16 +70,16 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; false
 
-const expression2 = a || b;
+const expression2 = a || b; true
 
-const expression3 = !a && b;
+const expression3 = !a && b; false
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); true
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; true
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); false
 
-const expression7 = a && a;
+const expression7 = a && a; true 
